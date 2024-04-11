1e36a2e1b55c8075346216bbfb5a0cea6e7ebca3
Joseph Gros <wiley.joseph.gros@gmail.com>
2024-04-11 11:35:49 +0200

    Some tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     const result = await response.json();
     console.log(result);
     const toArray : any = [];
-    toArray.push(result);
+    toArray.push(result, ...pokemonData);
     setPokemonData(toArray);
   }
 
@@ -29,14 +29,18 @@ function App() {
 
   return (
     <>
-      <input ref={inputRef} defaultValue={"pikachu"}></input>
-      <button onClick={() => handleClick()} className=''>Get Pokémon</button>
-      {pokemonData.map((pokemon: any) => (
-      <div key={pokemon.id} className='w-96'>
-        <img src={pokemon.sprites.front_default}></img>
-        <p>Name: {pokemon.name}</p>
-      </div>
-      ))}
+    <div className='flex flex-col justify-center'>
+      <input ref={inputRef} defaultValue={"pikachu"} className='rounded-md bg-gray-300 text-gray-950 m-2 p-2' placeholder='Search Pokémon'></input>
+        <button onClick={() => handleClick()} className='bg-red-200 text-gray-950 px-2 rounded-md m-2 p-2'>Get Pokémon</button>
+        {pokemonData.map((pokemon: any) => (
+        <div key={pokemon.id} className='flex flex-col justify-center w-52 h-52 bg-red-950 text-gray-950 m-2 p-2 rounded-md border-4 border-amber-300'>
+          <div className='flex justify-center bg-gray-300 w-40 rounded-md self-center m-2 border-2 border-amber-300'>
+            <img src={pokemon.sprites.front_default}></img>
+          </div>
+          <p className='self-center text-red-50'>Name: {pokemon.name}</p>
+        </div>
+        ))}
+    </div>
     </>
   )
 }
